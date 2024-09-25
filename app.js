5211718545d9524d855aa6da8b8f778bea264d21
Koushik Nandi <104435345+Nandikoushik@users.noreply.github.com>
2024-09-25 23:36:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(passport.initialize());
 app.use(fileUpload({ limits: { fileSize: 50 * 1024 * 1024 } }));
 
 app.use(cors({
-  origin: '*', methods: 'GET, POST, PUT, DELETE',
+  origin: 'https://crm-client-eight.vercel.app', methods: 'GET, POST, PUT, DELETE',
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
